3e7c3a47aeaba4f611e25ca9c4f29eca60fa3bdb
miyagis <22miyagi_shiho@axiz.co.jp>
2022-05-11 17:46:55 +0900

    演習問題7＿基本＿進捗率80％程度

---
+++ src/main/java/app/StartAppServlet.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StartAppServlet
+ */
+@WebServlet("/StartAppServlet")
+public class StartAppServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StartAppServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		   request.setCharacterEncoding("UTF-8");
+		    String name = request.getParameter("name");
+		
+		    String result = "未実施";
+		
+		    GameApp i = new GameApp("なにか");
+		    
+		    if (name != null && !name.isEmpty()) {
+		
+		    	result = i.start(name);
+		    	// このif分の中で、GameAppクラスのstartメソッドを呼び出し、
+		    	// 戻り値をresultに代入してください。
+		    }
+		  		doGet(request, response);
+	}
+
+}
--- src/main/webapp/appStart.jsp
+++ src/main/webapp/appStart.jsp
@@ -1,30 +1,13 @@
-<%--
-  GameAppクラス作成後、下記の記述が必要です。
-  <%@ page import="app.GameApp"%>
- --%>
-
 <%@ page language="java" contentType="text/html; charset=UTF-8"
   pageEncoding="UTF-8"%>
-<%
-    request.setCharacterEncoding("UTF-8");
-    String name = request.getParameter("name");
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
 
-    String result = "未実施";
-
-    GameApp i = new GameApp("なにか");
-
-    if (name != null && !name.isEmpty()) {
-
-      result = i.start(name);
-      // このif分の中で、GameAppクラスのstartメソッドを呼び出し、
-      // 戻り値をresultに代入してください。
-}
-%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Java応用_演習問題1</title>
+<title>Java応用_演習問題2</title>
 <style>
 body {
   border: solid 2px #000080;
@@ -41,16 +24,18 @@ body {
 </head>
 <body>
 
-  <h1>Java応用 - 演習問題1</h1>
+  <h1>Java応用 - 演習問題2</h1>
 
   <h2>ゲームアプリ実行ページ</h2>
 
+	<c:if test="${not empty result}">
   <div class="result">
     <h3>アプリの実行結果</h3>
-    <p><%=result%></p>
+    <p>${fn:escapeXml(result)}</p>
   </div>
+  </c:if>
 
-  <form action="appStart.jsp" method="post">
+  <form action="StartAppServlet" method="post">
     <label>ユーザ名：</label>
     <input type="text" name="name">
     <br>
